9eee25ac2cb9a57ed319767cd9e17304b0e2bfcd
HassanAmed <Hassan4621@gmail.com>
2021-12-26 17:24:23 +0500

    add contract for basic multi contract interactions

---
+++ contracts/interaction2contracts/ counters.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// SPDX-License-Identifier: MIT
+
+contract Counter {
+    uint public count;
+    
+    function increment() external {
+        count += 1;
+    }
+}
